643d5762604b9ddc2a6d77671d5fc6a2605a03e6
toan-le <levantoanbkit@gmail.com>
2017-10-25 13:34:55 +0700

    fix bug

--- client/src/app/account/account.js
+++ client/src/app/account/account.js
@@ -12,10 +12,10 @@ angular.module('account.index').config(['$routeProvider', 'securityAuthorization
 }]);
 angular.module('account.index').controller('AccountCtrl', [ '$scope',
   function($scope){
-    var socket = io.connect();
-    socket.on('data', function(obj) {
-      socket.emit('data', 'hello server');
-    });
+    // var socket = io.connect();
+    // socket.on('data', function(obj) {
+    //   socket.emit('data', 'hello server');
+    // });
     $scope.dayOfYear = moment().format('DDD');
     $scope.dayOfMonth = moment().format('D');
     $scope.weekOfYear = moment().format('w');
